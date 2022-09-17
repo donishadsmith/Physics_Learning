69ceebb1b364daea17871fde116cf7ab8c8a4cab
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2022-09-16 23:59:51 -0400

    Update Extracting_VAN-DMN_FCI.py

--- Extracting_VAN-DMN_FCI.py
+++ Extracting_VAN-DMN_FCI.py
@@ -21,7 +21,7 @@ subjects = layout.get(return_type='id', target='subject', suffix='bold')
 index = pd.MultiIndex.from_product([subjects, sessions, conditions])
 out_file = pd.DataFrame(index=index, columns=['SalVenAttn.Default.conn'])
 
-columns = pd.MultiIndex.from_product([ventAttn, dmnault])
+columns = pd.MultiIndex.from_product([ventAttn, default])
 van_dmn_node_conn = pd.DataFrame(index=index, columns=columns)
 
 for subject in subjects:
@@ -37,7 +37,7 @@ for subject in subjects:
                                     index_col=0)
                  #Calculating mean of all nodes for the VAN and DMN for the between-network connectivity measure.
                 graph.columns = graph.columns.astype(int)
-                van_dmn_graph = graph.loc[dmnault][ventAttn]
+                van_dmn_graph = graph.loc[default][ventAttn]
                 out_file.at[(subject, session, condition), 'SalVenAttn.Defnault.conn'] = np.mean(van_dmn_graph.mean())
                 out_file.to_csv(f'{deriv_dir}/idconn-v0.1-presub+90.g6973d15/fci_van-dmn_network-connectivity.tsv', sep='\t')
                 #Obtaining connectivity values for all nodes in VAN and DMN to create average within-network connectivity values using R script.
