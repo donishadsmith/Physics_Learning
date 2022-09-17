a9f924a48226fa8f4aa0bb629590c47d14b48404
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2022-09-16 23:59:23 -0400

    Update Extract_DAN-DMN_Connectivity_FCI.py

--- Extract_DAN-DMN_Connectivity_FCI.py
+++ Extract_DAN-DMN_Connectivity_FCI.py
@@ -7,7 +7,7 @@ deriv_dir = '/home/data/nbc/Laird_PhysicsLearning/dset-v2.0.0/derivatives'
 #Link to text file containing all 400 nodes. 
 labels = pd.read_csv('/home/dsmit216/schaefer400.txt', sep='\t', header=0, index_col=0)
 dorsAttn = labels[labels['Network'] == 'DorsAttn'].index
-dmnault = labels[labels['Network'] == 'Dmnault'].index
+dmnault = labels[labels['Network'] == 'Default'].index
 labels.rename({'Unnamed: 2': '#'}, axis=1, inplace=True)
 #Dmnine task, session, and file location for IDConn.
 sessions = [1,2]
@@ -18,9 +18,9 @@ layout = bids.BIDSLayout('/home/data/nbc/Laird_PhysicsLearning/dset-v2.0.0', der
 subjects = layout.get(return_type='id', target='subject', suffix='bold')
 
 index = pd.MultiIndex.from_product([subjects, sessions, conditions])
-out_file = pd.DataFrame(index=index, columns=['DorsAttn.Dmnault.conn'])
+out_file = pd.DataFrame(index=index, columns=['DorsAttn.Default.conn'])
 
-columns = pd.MultiIndex.from_product([dorsAttn, dmnault])
+columns = pd.MultiIndex.from_product([dorsAttn, default])
 dan_dmn_node_conn = pd.DataFrame(index=index, columns=columns)
 
 for subject in subjects:
@@ -36,7 +36,7 @@ for subject in subjects:
                                     index_col=0)
                  #Calculating mean of all nodes for the DAN and DMN for the between-network connectivity measure.
                 graph.columns = graph.columns.astype(int)
-                dan_dmn_graph = graph.loc[dmnault][dorsAttn]
+                dan_dmn_graph = graph.loc[default][dorsAttn]
                 out_file.at[(subject, session, condition), 'DorsAttn.Dmnault.conn'] = np.mean(dan_dmn_graph.mean())
                 out_file.to_csv(f'{deriv_dir}/idconn-v0.1-presub+90.g6973d15/fci_dan-dmn_network-connectivity.tsv', sep='\t')
                 #Obtaining connectivity values for all nodes in DAN and DMN to create average within-network connectivity values using R script.
